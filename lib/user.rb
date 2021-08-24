39d7ba4ea904b387699db2aea32eb442847ceb71
eballantine <lizzy.kayballantine@gmail.com>
2021-08-24 16:54:30 +0100

    user emails unique. users defined as one to many to bookings and listings

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  has_many :booking
-  has_many :listing
+  has_many :booking # describes user to bookings is a one to many relationship
+  has_many :listing # describes user to listings is a one to many relationship
 end
