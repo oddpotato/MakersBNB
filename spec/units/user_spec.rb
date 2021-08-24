c4cbc10a637e4bbfdd73b97c3ac07fe9683ff05d
eballantine <lizzy.kayballantine@gmail.com>
2021-08-24 16:13:17 +0100

    can create a new user

--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -9,4 +9,15 @@ describe User do
       expect(subject).to respond_to(:password_digest)
     end
   end
+  
+  context 'with valid input' do
+    describe '#self.create' do
+      it 'returns a new user' do
+        test_sign_up = described_class.create(name: 'Catty', email: 'McCat@face.com', password_digest: 'P@s2word123')
+        expect(test_sign_up).to be_a(User)
+        expect(test_sign_up.name).to eq 'Catty'
+        expect(test_sign_up.email).to eq 'McCat@face.com'
+      end
+    end
+  end
 end
