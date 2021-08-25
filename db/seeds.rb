4827f430c8ecbf59e88fa80cf2154960ad529360
Fiona Apps <wikipanyd@gmail.com>
2021-08-25 14:58:19 +0100

    Adding seed file

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+require 'require_all'
+require 'sinatra/activerecord'
+require_all 'lib'
+
+users = [
+  { name: 'S. A. McQuanzie',
+    email: 'mcquanzie@example.com',
+    password_digest: 'V3ryS3cur3P4ssw0rd'
+  },
+
+  { name: 'David',
+    email: 'superdave__123@hotmail.com',
+    password_digest: 'V3ryS3cur3P4ssw0rd'
+  }
+]
+
+users.each { |user| User.create(user) }
+Listing.create({ title: 'Hobbit Hole', description: 'It\'s very nice here', dates_available: [2021-8-31], user_id: 1 })
