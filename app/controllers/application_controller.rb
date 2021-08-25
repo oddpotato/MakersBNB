60ef793340c1ab054c61161025c78efb3735ffbd
Farya <farya.hsn@gmail.com>
2021-08-25 13:21:49 +0200

    setting up localhost for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
-require 'sinatra'
+require "sinatra"
+require "sinatra/activerecord"
+require "sinatra/base"
+require "sinatra/reloader" if development?
+require "sinatra/flash"
+
 class ApplicationController < Sinatra::Base
 
-end
+    configure :development do
+      register Sinatra::Reloader
+    end
+  
+    enable :sessions
+    register Sinatra::Flash
+  
+    get '/' do
+      'Hello People!'
+    end 
+  
+    run! if app_file == $0
+  end
+
