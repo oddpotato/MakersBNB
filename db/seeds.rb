b210de94b86409ebc1fb82387977f9c349142ad8
Fiona Apps <wikipanyd@gmail.com>
2021-08-25 15:31:01 +0100

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,4 @@ users = [
 
 users.each { |user| User.create(user) }
 Listing.create({ title: 'Hobbit Hole', description: 'It\'s very nice here', dates_available: [2021-8-31], user_id: 1 })
+Booking.create({ user_id: 2, listing_id: 1, date_booked: 2021-8-31, confirmed: TRUE })
