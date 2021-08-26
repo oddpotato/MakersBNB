79897ca1060f687bbcc210f285296b26e6d2bed7
eballantine <lizzy.kayballantine@gmail.com>
2021-08-26 14:48:55 +0100

    navigation to sign up from login page

--- spec/features/login_page_spec.rb
+++ spec/features/login_page_spec.rb
@@ -1,8 +1,13 @@
-feature "access login page from home" do
- scenario "user clicks login link" do
-  visit('/')
-  click_on('Login')
-  expect(page).to have_content 'Login To MakersBNB'
- end
+feature 'access login page from home' do
+  scenario 'user clicks login link' do
+    visit '/'
+    click_on('Login')
+    expect(page).to have_content 'Login To MakersBNB'
+  end
 
+  scenario 'user can navigate to sign up' do
+    visit '/sessions/new'
+    click_on('Sign Up')
+    expect(page).to have_content('Sign Up To MakersBNB')
+  end
 end
