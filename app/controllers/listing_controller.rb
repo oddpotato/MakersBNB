66c436f148599f031bc25ce69ace072171aaab39
Fiona Apps <wikipanyd@gmail.com>
2021-08-26 16:19:53 +0100

    playing with controller

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,6 +1,10 @@
 class ListingController < ApplicationController
 
-@date = Date.today
+get (/listings) { erb :'listings/index' }
 
+def index
+    @date = Date.today
+    @answer = "Hi how are ya!?"
+end
 end
 
