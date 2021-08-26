f43ffcfada644629528a593da5abd73b2ce3e4b1
Fiona Apps <wikipanyd@gmail.com>
2021-08-26 17:15:33 +0100

    IT WORKS! Controller talking to pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,45 +42,8 @@ class ApplicationController < Sinatra::Base
       end
     end
 
-    get '/listings' do
-      @listings = Listing.all
-      # @available_from = params[:available_from] || "2021-08-26"
-      # @available_to = params[:available_to] || "2021-08-27"
-      # @date_range = [*@available_from..@available_to]
-      # @result = Listing.where(dates_available: @date_range)
-      # # @result = Listing.where(:dates_available => @date_range)
-      # # p result
-      # # @result = Listing.select(:dates_available)
-      # # p "hello"
-      # p @result
-      erb :'listings/index'
-    end
-
-    get '/listings/new' do
-      erb :'listings/new'
-    end
-
-    post '/listings/new' do
-  
-      Listing.create(
-        user_id: session[:user_id], 
-        price: params[:list_price], 
-        title: params[:list_title], 
-        description: params[:list_desc], 
-        dates_available: params[:list_date]
-      )
-      redirect('/listings')
-    end
-  
-    post '/listings/:id' do
-      @listings_id = params[:id]
-      @listing = Listing.find_by(id: @listings_id)
-      erb :'listings/view'
-    end
-    
     post "/book" do
-      user_id = session[:user_id]
-      booking = Booking.create(user_id: user_id, listing_id: params[:listing_id], date_booked: params[:booking_date], confirmed: nil)
+      booking = Booking.create(user_id: params[:user_id], listing_id: params[:listing_id], date_booked: params[:booking_date], confirmed: nil)
       redirect('/requests')
     end
 
@@ -90,17 +53,12 @@ class ApplicationController < Sinatra::Base
       
     post '/signup' do
     # check email isn't already in DB
-    @email = params[:email]
-      if User.exists?(email: @email)
-        flash[:notice1] = 'Email already in use'
-        redirect('/')
-      else  # assuming no...
-        user = User.create(name: params[:name], email: @email, password_digest: params[:password])
-        session[:user_id] = user.id
-        redirect('/listings')
-      end
+    # assuming no...
+      user = User.create(name: params[:name], email: params[:email], password_digest: params[:password])
+      erb :'listings/index'
     end
-  
+
+    
     run! if app_file == $0
   end
 
--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,9 +1,31 @@
 class ListingController < ApplicationController
 
-  get (/listings) { erb :'listings/index' }
+    get '/listings' do
+        @listings = Listing.all
+        @date = Time.now.to_date
+        erb :'listings/index'
+      end
+  
+      get '/listings/new' do
+        erb :'listings/new'
+      end
+  
+      post '/listings/new' do
+    
+        Listing.create(
+          user_id: session[:user_id], 
+          price: params[:list_price], 
+          title: params[:list_title], 
+          description: params[:list_desc], 
+          dates_available: params[:list_date]
+        )
+        redirect('/listings')
+      end
+    
+      post '/listings/:id' do
+        @listings_id = params[:id]
+        @listing = Listing.find_by(id: @listings_id)
+        erb :'listings/view'
+      end
 
-  def index
-      @date = Date.today
-      @answer = "Hi how are ya!?"
-  end
 end
