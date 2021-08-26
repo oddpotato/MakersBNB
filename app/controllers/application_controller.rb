b2f929e371ac0155bfbfc7535bbb961caaae50ca
Ahsan <rasul.ahsan@gmail.com>
2021-08-26 15:30:16 +0100

    user_id set to session[:user_id]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -79,7 +79,8 @@ class ApplicationController < Sinatra::Base
     end
     
     post "/book" do
-      booking = Booking.create(user_id: params[:user_id], listing_id: params[:listing_id], date_booked: params[:booking_date], confirmed: nil)
+      user_id = session[:user_id]
+      booking = Booking.create(user_id: user_id, listing_id: params[:listing_id], date_booked: params[:booking_date], confirmed: nil)
       redirect('/requests')
     end
 
