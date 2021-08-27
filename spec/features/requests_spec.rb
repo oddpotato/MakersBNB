9aa361c502c031b5839a2985fc7a1802c107911a
Farya <farya.hsn@gmail.com>
2021-08-27 12:55:06 +0200

    features tests for requests page

--- spec/features/requests_spec.rb
+++ spec/features/requests_spec.rb
@@ -3,7 +3,18 @@ feature 'requests page' do
     login_abby
     visit('/requests')
     expect(page).to have_content('Hobbit Hole')
+    expect(page).to have_content('2021-12-26')
+    expect(page).to have_content('true')
+   
+  end  
+
+  scenario 'user can see requests on their listings' do
+    login_abby
+    visit('/requests')
+    expect(page).to have_content('Downing Street')
     expect(page).to have_content('2021-09-21')
+    expect(page).to have_content('null')
+  
   end  
 
 end
