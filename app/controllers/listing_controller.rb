3d8126641362ef556cb85e846a380b7ae30357e5
Fiona Apps <wikipanyd@gmail.com>
2021-08-27 08:10:09 +0100

    lots of progress on filtering by date

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,12 +1,33 @@
+require './lib/listing.rb'
+
 class ListingController < ApplicationController
 
-
+    def filter_listings(start_date = @start_date, end_date = @end_date)
+        @list_of_filter_listings = []
+        @listings.each do |listing|
+          if (listing.start_date > start_date) || (listing.end_date < end_date)
+            filter_listings.append(listing)
+          end
+        end
+    end
 
     get '/listings' do
         @listings = Listing.all
         @date = Time.now.to_date
         erb :'listings/index'
       end
+
+
+      post '/listings/filtered' do
+        @listings = Listing.all
+        @date = Time.now.to_date
+        @start_date = params[:start_date]
+        @end_date = params[:end_date] 
+        @filter_listings
+        @list_of_filter_listings
+        erb :'listings/index'
+      end
+
 
       get '/listings/new' do
         erb :'listings/new'
